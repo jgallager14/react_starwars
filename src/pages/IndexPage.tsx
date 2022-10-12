5609235ab0c6aaf3551cbd17b31320f61c66bbfb
jgallager14 <106274758+jgallager14@users.noreply.github.com>
2022-10-12 16:28:36 -0600

    removed redundancies, outer div to flex for sizing

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,32 +1,32 @@
 export function IndexPage() {
   return (
-    <div className="w-screen h-screen">
-      <header className="w-screen py-8 flex items-center justify-around bg-gray-200 border-b-solid border-b-2 border-b-black">
+    <div className="h-screen flex flex-col">
+      <header className="py-8 flex items-center justify-around bg-gray-200 border-b-solid border-b-2 border-b-black">
         <h1 className="text-3xl">Star Wars Info Hub</h1>
       </header>
-      <div className="flex w-screen h-full">
-        <nav className="h-full basis-1/5 flex flex-col bg-gray-500">
-          <a className="pt-8 pb-8 pl-8 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
+      <div className="flex h-full">
+        <nav className="h-full w-1/5 min-w-min flex flex-col bg-gray-500">
+          <a className="p-6 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
             People
           </a>
-          <a className="pt-8 pb-8 pl-8 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
+          <a className="p-6 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
             Films
           </a>
-          <a className="pt-8 pb-8 pl-8 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
+          <a className="p-6 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
             Spaceships
           </a>
-          <a className="pt-8 pb-8 pl-8 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
+          <a className="p-6 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
             Vehicles
           </a>
-          <a className="pt-8 pb-8 pl-8 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
+          <a className="p-6 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
             Species
           </a>
-          <a className="pt-8 pb-8 pl-8 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
+          <a className="p-6 align-middle bg-gray-300 border-b-2 border-b-black border-b-solid text-xl">
             Planets
           </a>
         </nav>
-        <main className="basis-4/5 flex items-center justify-center">
-          <p className="text-xl">
+        <main className="w-4/5 flex items-center justify-center">
+          <p className="text-xl p-4">
             Welcome, please select a resource to proceed!
           </p>
         </main>
