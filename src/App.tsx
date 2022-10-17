77fd77b7cc0706001b3cc0612f149279277b50dc
jgallager14 <106274758+jgallager14@users.noreply.github.com>
2022-10-17 10:44:48 -0600

    route tree created for CSR

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
 import { IndexPage } from "./pages/IndexPage";
+import { Greeting } from "./components/Content/SelectedPage";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IndexPage />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Greeting displayText="Welcome, please select a resource to continue!" />
+        ),
+      },
+      {
+        path: "/movies/",
+        element: <Greeting displayText="Movies" />,
+      },
+      {
+        path: "/planets/",
+        element: <Greeting displayText="Planets" />,
+      },
+      {
+        path: "/ships/",
+        element: <Greeting displayText="Ships" />,
+      },
+      {
+        path: "/species/",
+        element: <Greeting displayText="Species" />,
+      },
+      {
+        path: "/vehicles/",
+        element: <Greeting displayText="Vehicles" />,
+      },
+      {
+        path: "/characters/",
+        element: <Greeting displayText="Characters" />,
+      },
+    ],
+  },
+]);
 
 export function App() {
-  return <IndexPage />;
+  return <RouterProvider router={router} />;
 }
