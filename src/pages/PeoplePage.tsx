ca5ba8d7df42c4a640304ec7b1a776aa4f415834
jgallager14 <106274758+jgallager14@users.noreply.github.com>
2022-10-19 15:28:15 -0600

    updated PeoplePage

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -1,10 +1,10 @@
-import { SetStateAction, useEffect, useState } from "react";
-import { SwapiResources } from "../utils/swapiHelpers";
-import { fetchResource } from "../components/Content/SelectedPage";
+import { useEffect, useState } from "react";
+import { SwapiResources, fetchResource, Person } from "../utils/swapiHelpers";
 
 export function PeoplePage(): JSX.Element {
   const [isLoading, updateIsLoading] = useState<boolean>(true);
-  const [fetchResults, updateFetchResults] = useState([]);
+  const [fetchResults, updateFetchResults] = useState<Person[]>([]);
+  const [showMore, updateShowMore] = useState<boolean>(false);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -18,7 +18,7 @@ export function PeoplePage(): JSX.Element {
     return () => {
       abortController.abort();
     };
-  }, []);
+  }, [showMore]);
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -26,23 +26,45 @@ export function PeoplePage(): JSX.Element {
 
   return (
     <div className="m-10">
-      <p className="text-3xl text-center">People</p>
-      <ul className="grid grid-cols-4 gap-10">
+      <h2 className="text-3xl text-center">People</h2>
+      <div className="grid grid-cols-4 gap-10">
         {fetchResults.map((result) => {
-          return (
-            <li>
-              <ul>
-                <li className="text-lg font-bold">{result.name}</li>
-                <li>Height: {result.height}cm</li>
-                <li>Weight: {result.mass}kg</li>
-                <li>Hair: {result.hair_color}</li>
-                <li>{result.gender}</li>
-                <li>Born in {result.birth_year}</li>
-              </ul>
-            </li>
-          );
+          if (showMore === false) {
+            return (
+              <section>
+                <h3 className="text-lg font-bold">{result.name}</h3>
+                <button
+                  onClick={() => {
+                    updateShowMore(true);
+                  }}
+                >
+                  Show More
+                </button>
+              </section>
+            );
+          } else {
+            return (
+              <section>
+                <h3 className="text-lg font-bold">{result.name}</h3>
+                <ul>
+                  <li>Height: {result.height}cm</li>
+                  <li>Weight: {result.mass}kg</li>
+                  <li>Hair: {result.hair_color}</li>
+                  <li>{result.gender}</li>
+                  <li>Born in {result.birth_year}</li>
+                </ul>
+                <button
+                  onClick={() => {
+                    updateShowMore(false);
+                  }}
+                >
+                  Show Less
+                </button>
+              </section>
+            );
+          }
         })}
-      </ul>
+      </div>
     </div>
   );
 }
