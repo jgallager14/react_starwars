622b2997d4bb0f6b3a41b5c5251ff92e80ea1928
jgallager14 <106274758+jgallager14@users.noreply.github.com>
2022-10-25 13:10:27 -0600

    useFetchOnMount now fetches and returns a list of all results, not just the first 10

--- src/hooks/useFetchOnMount.ts
+++ src/hooks/useFetchOnMount.ts
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 
 export function useFetchOnMount<T>(apiEndpoint: string) {
-  const [data, updateData] = useState<T>();
+  const [data, updateData] = useState<T[]>();
 
   useEffect(() => {
     const abortController = new AbortController();
 
     async function fetchData() {
-      const response = await fetch(apiEndpoint, {
-        signal: abortController.signal,
-      });
-      const data = await response.json();
-      updateData(data);
+      let dataArray: T[] = [];
+      let currentPage = apiEndpoint;
+      while (currentPage !== null) {
+        const response = await fetch(currentPage, {
+          signal: abortController.signal,
+        });
+        const data = await response.json();
+        for (let i = 0; i < data.results.length; i++) {
+          dataArray.push(data.results[i]);
+        }
+        currentPage = data.next;
+      }
+      updateData(dataArray);
     }
 
     fetchData();
